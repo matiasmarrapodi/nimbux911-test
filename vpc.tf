7f204c8dc7fcf84a82ffe74bae722343ae4d6e3c
matiasmarrapodi <matias.marrapodi91@gmail.com>
2022-03-13 18:02:52 -0300

    fix

--- vpc.tf
+++ vpc.tf
@@ -84,7 +84,7 @@ resource "aws_route_table_association" "public_rt_asso" {
 }
 
 resource "aws_route_table" "private_rt" {
-  vpc_id = aws_vpc.servers_vpc.id
+  vpc_id = aws_vpc.laboratorio_vpc.id
   route {
     cidr_block = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.nat_gateway.id
