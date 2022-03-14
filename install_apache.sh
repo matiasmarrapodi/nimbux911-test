cd800b6d9208971ddf6400d0a68178c561b37f95
matiasmarrapodi <matias.marrapodi91@gmail.com>
2022-03-14 18:09:19 -0300

    msj apache

--- install_apache.sh
+++ install_apache.sh
@@ -6,4 +6,4 @@ sudo apt update -y
 sudo apt install apache2 -y
 echo "Apache2 install ok"
 sudo service apache2 start
-echo "Hello World from $(hostname -f)" > /var/www/html/index.html
+echo "HI NIMBUXERS THIS IS APACHE2" > /var/www/html/index.html
